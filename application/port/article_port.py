ec11e0c75335ff60051cc58c1869440e3736c32d
Keisuke Ota <k.ota109@outlook.com>
2025-07-19 16:44:19 +0900

    記事を取得するportを作成

---
+++ application/port/article_port.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+from typing import List
+from domain.item import Item
+
+
+class IArticleRepository(ABC):
+    TARGET_PAGE_COUNT: int = 10
+
+    @abstractmethod
+    def fetch_items(self, page: int) -> List[Item]:
+        pass
